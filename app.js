3cbbe9e3c71df2a6ac4495ab8840cf5558dadf51
Vanessa Bragdon <vanessabragdon@Vanessas-MacBook-Pro.local>
2022-03-04 10:30:59 -1000

    random color js in master branch

---
+++ app.js
@@ -0,0 +1,13 @@
+function randomRGB() {
+   const r = Math.floor(Math.random() * 256)
+   const g = Math.floor(Math.random() * 256)
+   const b = Math.floor(Math.random() * 256)
+   return `rgb(${r},${g},${b})`
+}
+
+const letters = document.querySelectorAll('.letter')  //class is called by dot method, .letter
+setInterval(function () {
+   for (let letter of letters) {
+      letter.style.color = randomRGB()
+   }
+}, 1000)
